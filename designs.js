ae1447e56eb236eeecfc52d059b363d1ec255ad2
memsjr <82689739+memsjr@users.noreply.github.com>
2022-07-13 08:25:27 -0700

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,34 @@
+var height, width, color;
+
+$('#sizeSelect').submit(function (event){
+  event.preventDefault();
+  height = $('#inputHeight').val();
+  width  = $('#inputWidth').val();
+  makeGrid(height, width);
+
+});
+
+function makeGrid(x,y) {
+  $('tr').remove();
+
+  for(var i=1; i<=x; i++)
+    {
+      $('#pixelCanvas').append('<tr id=table'+ i + '></tr>');
+  for(var j=1; j<=y; j++)
+    {
+      $('#table' + i).append('<td></td>');
+    }
+    }
+
+  $('td').click(function addColor()
+   {
+    color = $('#colorSelect').val();
+    if($(this).attr('style'))
+      {
+        $(this).removeAttr('style');
+      }
+    else
+      $(this).attr('style','background-color:'+color);
+  })
+
+}
